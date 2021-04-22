236ebfa973b82825d94ee5b5c935eb611660e8c4
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-04-22 17:42:12 -0300

    pasta JavaScript_01 com os exercicios do conteudo part4 feito

---
+++ JavaScript_01/Part_4/script.js
@@ -0,0 +1,9 @@
+let nota = 79
+
+if (nota < 60) {
+  console.log('Você foi reprovada(o)')
+} else if (nota < 80){
+  console.log('Você está na nossa lista de espera')
+} else {
+  console.log('Parabéns, você foi aprovada(o)')
+}
