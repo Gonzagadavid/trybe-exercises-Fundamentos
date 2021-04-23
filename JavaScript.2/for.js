88c208510f12898f5af82a4fd0ba84baa0742802
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-04-23 15:29:40 -0300

    feito exercicios do conteudo na pasta JavaScript.2

---
+++ JavaScript.2/for.js
@@ -0,0 +1,6 @@
+// Utilize o for para imprimir os elementos da lista groceryList com o console.log() :
+const groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia']
+
+for (let i = 0; i < groceryList.length; i++) {
+  console.log(groceryList[i])
+}
