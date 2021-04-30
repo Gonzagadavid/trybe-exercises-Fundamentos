722d79a8870ad6fe36848bf9b6f3af277f2b1871
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-04-30 20:17:56 -0300

    adiconado na pasta JavaScript_5.2 na pasta exercices e feito os itens 4, 5 e 6

--- exercices/JavaScript_5.2/script.js
+++ exercices/JavaScript_5.2/script.js
@@ -14,8 +14,20 @@ div2.className = 'center-content';
 div.appendChild(div2);
 
 // Adicione a tag p como filho do div criado no passo 3 e coloque algum texto;
+const p = document.createElement('p');
+p.innerHTML =
+  '"Deixe as portas abertas.<br>Permita que seus pensamentos venham e vão.<br>Só não sirva chá a eles".<br>Shunryu Suzuki (1904 - 1971)';
+div2.appendChild(p);
+
 // Adicione a tag div com a classe left-content como filho da tag div criada no passo 2;
+const div3 = document.createElement('div');
+div.className = 'left-content';
+div.appendChild(div3);
+
 // Adicione a tag div com a classe right-content como filho da tag div criada no passo 2;
+const div4 = document.createElement('div');
+div.className = 'right-content';
+div.appendChild(div4);
 // Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do div criado no passo 5;
 // Adicione uma lista não ordenada com os valores de 1 a 10 por extenso como valores da lista. Essa lista deve ser filha do div criado no passo 6;
 // Adicione 3 tags h3 , todas sendo filhas do div criado no passo 2.
