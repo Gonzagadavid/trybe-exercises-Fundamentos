bca7e0480b63a8e800feb27df94b9abf083f93be
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-05-03 16:10:57 -0300

    adicionada pasta JavaScript_5.3 na pasta exercices_content com os exercicios do conteudo feito e algums itens além do que foi pedido no conteudo incrementado

--- exercices/JavaScript_5.2/script.js
+++ exercices/JavaScript_5.2/script.js
@@ -80,18 +80,3 @@ div2.style.backgroundColor = 'green';
 // 15- Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8.
 lista.lastChild.remove();
 lista.lastChild.remove();
-// Adicione a tag div com a classe main-content como filho da tag body ;
-// Adicione a tag div com a classe center-content como filho da tag div criada no passo 2;
-// Adicione a tag p como filho do div criado no passo 3 e coloque algum texto;
-// Adicione a tag div com a classe left-content como filho da tag div criada no passo 2;
-// Adicione a tag div com a classe right-content como filho da tag div criada no passo 2;
-// Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do div criado no passo 5;
-// Adicione uma lista não ordenada com os valores de 1 a 10 por extenso como valores da lista. Essa lista deve ser filha do div criado no passo 6;
-// Adicione 3 tags h3 , todas sendo filhas do div criado no passo 2.
-// Agora que você criou muita coisa, vamos fazer algumas alterações e remoções:
-// Adicione a classe title na tag h1 criada;
-// Adicione a classe description nas 3 tags h3 criadas;
-// Remova o div criado no passo 5 (aquele que possui a classe left-content ). Utilize a função .removeChild() ;
-// Centralize o div criado no passo 6 (aquele que possui a classe right-content ). Dica: para centralizar, basta configurar o margin-right: auto do div ;
-// Troque a cor de fundo do elemento pai da div criada no passo 3 (aquela que possui a classe center-content ) para a cor verde;
-// Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8.
