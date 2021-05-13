acf22285a63fdd6040b7ca169718852bc473d078
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-05-13 15:24:05 -0300

    refatorado exercicios da pasta HMTL-CSS_6.1 na pasta exercices

--- exercices/HTML-CSS_6.1/script.js
+++ exercices/HTML-CSS_6.1/script.js
@@ -1,15 +1,3 @@
-const nome = document.getElementById('nome');
-const email = document.getElementById('email');
-const cpf = document.getElementById('cpf');
-const endereco = document.getElementById('endereco');
-const cidade = document.getElementById('cidade');
-const estado = document.getElementById('estado');
-const casa = document.getElementById('casa');
-const apartamento = document.getElementById('apartamento');
-const curriculo = document.getElementById('curriculo');
-const cargo = document.getElementById('cargo');
-const decricaoCargo = document.getElementById('decricao-cargo');
-const dataInicio = document.getElementById('data-inicio');
 const btnSubmit = document.getElementById('btn-submit');
 const curriculoContainer = document.getElementById('curriculo-container');
 const btnClear = document.getElementById('clear');
@@ -17,6 +5,7 @@ const erros = document.createElement('ul');
 const erroContainer = document.getElementById('erro-container');
 const regexDate = /([0-2][0-9]|[3][01])-(0[1-9]|1[0-2])-[0-9]{2}/g;
 const regexCPF = /\d{11}/g;
+const regexEmail = /.+@.+/g;
 const selectionState = document.getElementById('estado');
 const url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
 const myHeaders = new Headers();
@@ -49,59 +38,56 @@ let message = false;
 function checkEmpty(valor, msg) {
   if (valor === '') {
     const li = document.createElement('li');
-    li.innerHTML = `o campo ${msg} é obrigatório`;
+    li.innerHTML = `O campo ${msg} é obrigatório`;
     erros.appendChild(li);
     error = true;
   }
 }
 
-function checkFormat(teste, msg) {
-  if (teste) {
-    const li = document.createElement('li');
-    li.innerHTML = msg;
-    erros.appendChild(li);
-    error = true;
-  }
-}
+const arrayIds = [
+  { id: 'nome', regex: false },
+  { id: 'email', regex: regexEmail },
+  { id: 'cpf', regex: regexCPF },
+  { id: 'cidade', regex: false },
+  { id: 'cargo', regex: false },
+  { id: 'decricao-cargo', regex: false },
+  { id: 'data-inicio', regex: regexDate },
+  { id: 'endereco', regex: false },
+  { id: 'resumo-curriculo', regex: false },
+];
 
 function checkError() {
-  checkEmpty(nome.value, 'nome');
-  checkEmpty(endereco.value, 'endereço');
-  checkEmpty(cidade.value, 'cidade');
-  checkEmpty(cargo.value, 'cargo');
-  checkEmpty(decricaoCargo.value, 'descrição do cargo');
+  for (let index = 0; index < arrayIds.length; index += 1) {
+    const element = document.querySelector(`#${arrayIds[index].id}`);
+    const msg = arrayIds[index].id.replace(/\-/g, ' ');
+    checkEmpty(element.value, msg);
 
-  checkFormat(!email.value.includes('@'), 'email inválido');
-  checkFormat(!regexCPF.test(cpf.value), 'use apenas digitos no campo do cpf');
-  checkFormat(!regexDate.test(dataInicio.value), 'data com o formato inválido');
-  checkFormat(
-    curriculo.value === 'Resumo do currículo',
-    'o campo resumo do curriculo é obrigatório'
-  );
+    if (arrayIds[index].regex && !arrayIds[index].regex.test(element.value)) {
+      const li = document.createElement('li');
+      li.innerHTML = `Formato invalido para ${msg}`;
+      erros.appendChild(li);
+      error = true;
+    }
+  }
+
   return error;
 }
 
 function renderErros() {
-  console.log('input');
   erroContainer.appendChild(erros);
+  window.scrollTo(0, 0);
 }
 
 function renderCurriculo() {
-  curriculoContainer.innerHTML = `
-    Nome: ${nome.value}<br>     
-    cpf: ${cpf.value}<br>
-    Email: ${email.value}<br>
-    Endereço: ${endereco.value}<br>
-    Cidade: ${cidade.value} Estado: ${estado.value}<br>
-    Residencia: ${apartamento.value === '' ? casa.value : apartamento.value}<br>
-    Resumo do curriculo:<br>
-    ${curriculo.value}<br>
-    Cargo: ${cargo.value} 
-    Data do inicio: ${dataInicio.value}<br><br>
-    Descrição do cargo:<br>
-    ${decricaoCargo.value}<br>
-  `;
+  for (let index = 0; index < arrayIds.length; index += 1) {
+    const p = document.createElement('p');
+    const element = document.querySelector(`#${arrayIds[index].id}`);
+    const msg = arrayIds[index].id.replace(/\-/g, ' ');
+    p.innerHTML = `${msg}: ${element.value}\n`;
+    curriculoContainer.appendChild(p);
+  }
 }
+
 function submitForm(event) {
   event.preventDefault();
   erros.innerHTML = '';
