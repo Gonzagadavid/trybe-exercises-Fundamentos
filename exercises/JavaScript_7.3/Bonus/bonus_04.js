8dc25f5901bddf16e06546da5612788d2288ae00
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-05-22 14:34:59 -0300

    na pasta JavaScript_7.3 feito o quarto exercicio da parte bonus

---
+++ exercises/JavaScript_7.3/Bonus/bonus_04.js
@@ -0,0 +1,20 @@
+// Use a variável parameter como parâmetro da função abaixo, escreva testes para verificar se a mesma está retornando
+// como se vê na variável result e, caso não esteja, altere o código para que ele passe nos testes.
+// Lembre-se: testes pequenos e numerosos! Escreva um por vez e vá corrigindo a função aos poucos.
+const assert = require('assert');
+
+const getLargestNumber = (array) => {
+  let largestNumber = array[0];
+  for (let index = 1; index < array.length; index += 1) {
+    if (array[index] > largestNumber) {
+      largestNumber = array[index];
+    }
+  }
+  return largestNumber;
+};
+
+const parameter = [45, 8, 2, 50, 1, 7, 99];
+const result = 99;
+
+assert.strictEqual(typeof getLargestNumber(parameter), 'number');
+assert.deepStrictEqual(getLargestNumber(parameter), result);
