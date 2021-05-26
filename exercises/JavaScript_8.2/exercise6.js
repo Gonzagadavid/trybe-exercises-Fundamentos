7b0e20ae49ecb8870427cae36c13e58250d5550a
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-05-26 20:47:42 -0300

    feito exrcise6 dos exercicios 8.2

---
+++ exercises/JavaScript_8.2/exercise6.js
@@ -0,0 +1,9 @@
+import books from './books.js';
+import assert from 'assert';
+
+// Faça uma função que retorne true , se algum livro foi lançado na década de 80, e false , caso contrário.
+const expectedResult = true;
+
+const someBookWasReleaseOnThe80s = () => books.some((book) => book.releaseYear >= 1980 && book.releaseYear < 1990);
+
+assert.strictEqual(someBookWasReleaseOnThe80s(), expectedResult);
