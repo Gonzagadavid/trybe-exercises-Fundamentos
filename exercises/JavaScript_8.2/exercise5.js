90406165f165dfddfb9772688dbe03fd3e13abe3
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-05-26 20:44:13 -0300

    feito exrcise5 dos exercicios 8.2

---
+++ exercises/JavaScript_8.2/exercise5.js
@@ -0,0 +1,9 @@
+import books from './books.js';
+import assert from 'assert';
+
+// Faça uma função que retorne true , se todas as pessoas autoras nasceram no século XX, ou false , caso contrário.
+const expectedResult = false;
+
+const everyoneWasBornOnSecXX = () => books.every((book) => Math.floor(book.author.birthYear / 100) === 19);
+
+assert.strictEqual(everyoneWasBornOnSecXX(), expectedResult);
