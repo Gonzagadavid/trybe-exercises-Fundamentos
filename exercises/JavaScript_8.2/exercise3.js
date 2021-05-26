692e98d252ffb4e8285826c8278794fa26d2fa47
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-05-26 20:37:20 -0300

    feito exrcise3 dos exercicios 8.2

---
+++ exercises/JavaScript_8.2/exercise3.js
@@ -0,0 +1,18 @@
+import books from './books.js';
+import assert from 'assert';
+
+// Encontre o primeiro livro cujo nome possui 26 caracteres.
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+const getNamedBook = () => books.find((book) => book.name.length === 26);
+
+assert.deepStrictEqual(getNamedBook(), expectedResult);
