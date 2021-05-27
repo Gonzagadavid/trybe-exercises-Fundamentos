ff552e47619c687a531258b9f6560b2a9a3b8f95
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-05-27 12:25:32 -0300

    feito exercise4 dos exercicios 8.3

---
+++ exercises/JavaScript_8.3/exercise4.js
@@ -0,0 +1,36 @@
+const assert = require('assert');
+const { filter } = require('./books.js');
+const books = require('./books.js');
+
+// Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene-o pelo livro mais velho.
+// Dica: use as funções filter e sort
+
+const expectedResult = [
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+];
+
+const oldBooksOrdered = () => books
+  .filter(book => 2021 - book.releaseYear > 60)
+  .sort((a, b) => a.releaseYear - b.releaseYear) 
+
+assert.deepStrictEqual(oldBooksOrdered(), expectedResult);
