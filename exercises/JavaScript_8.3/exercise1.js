416796f432c20e4ff68d62b07c3ca734090e4226
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-05-27 12:11:24 -0300

    feito exercise1 dos exercicios 8.3

---
+++ exercises/JavaScript_8.3/exercise1.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const books = require('./books.js');
+
+// Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
+// Dica: Use a função map
+const expectedResult = [
+  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+  'Fundação - Ficção Científica - Isaac Asimov',
+  'Duna - Ficção Científica - Frank Herbert',
+  'A Coisa - Terror - Stephen King',
+  'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+];
+
+const formatedBookNames = () => books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`);
+
+assert.deepStrictEqual(formatedBookNames(), expectedResult);
