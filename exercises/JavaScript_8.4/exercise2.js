2650aa8d9843f461a2055c1fb9ea43b17af4b4c6
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-05-28 14:42:32 -0300

    refatorado exercise2 dos exercicios 8.4

--- exercises/JavaScript_8.4/exercise2.js
+++ exercises/JavaScript_8.4/exercise2.js
@@ -1,9 +1,9 @@
-const assert = require('assert')
-const books = require('./books.js')
+const assert = require('assert');
+const books = require('./books.js');
 
 // Crie uma string com os nomes de todas as pessoas autoras.
 const expectedResult = 'George R. R. MartinJ. R. R. TolkienIsaac AsimovFrank HerbertStephen KingH. P. Lovecraft';
 
-const averageAge = () => books.reduce((acc, crr) => acc + crr.author.name, '')
+const averageAge = () => books.reduce((acc, crr) => acc + crr.author.name, '');
 
 assert.strictEqual(averageAge(), expectedResult);
