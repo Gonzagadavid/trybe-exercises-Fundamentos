e579ee222c77e8f150ddb38e297e6f0c22278bbe
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-05-31 18:58:34 -0300

    feito exercise5 do 8.5

---
+++ exercises/JavaScript_8.5/exercise5.js
@@ -0,0 +1,15 @@
+// Escreva a função swap , que dado um array de 3 elementos, retorna um novo array com o primeiro e terceiro elementos
+// trocados. Detalhe: você precisa fazer essa função gastando 1 linha só:
+// Dica: use array destructuring .
+const assert = require('assert');
+
+const myList = [1, 2, 3];
+
+// escreva swap abaixo
+const swap = ([one, two, three]) => [three, two, one];
+
+const swappedList = swap(myList);
+
+assert.strictEqual(swappedList[0], 3);
+assert.strictEqual(swappedList[1], 2);
+assert.strictEqual(swappedList[2], 1);
