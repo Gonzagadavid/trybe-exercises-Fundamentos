86db01037c85781be3f231c375fef0d3af508d0f
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-05-31 16:50:41 -0300

    feito exercicio de fixação de Default Parameters do conteudo do dia

---
+++ exercises_content/JavaScript_8.5/defaultParameters.js
@@ -0,0 +1,5 @@
+// Para praticar, escreva uma função multiply que multiplique dois números passados como argumentos.
+// Atribua como default o valor 1 caso não seja passado nenhum valor como segundo parâmetro.
+const multiply = (number = 1, value = 1) => number * value;
+
+console.log(multiply(8));
