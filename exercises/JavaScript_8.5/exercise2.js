863fcb458a35d41de0ba8260a2bb02808357b971
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-05-31 18:36:56 -0300

    feito exercise2 do 8.5

---
+++ exercises/JavaScript_8.5/exercise2.js
@@ -0,0 +1,11 @@
+// Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos. Ou seja:
+// Dica: use parâmetro rest .
+const assert = require('assert');
+
+const sum = (...params) => params.reduce((accumulator, current) => accumulator + current, 0);
+
+assert.strictEqual(sum(), 0);
+assert.strictEqual(sum(1), 1);
+assert.strictEqual(sum(1, 2), 3);
+assert.strictEqual(sum(1, 2, 3), 6);
+assert.strictEqual(sum(1, 2, 3, 4), 10);
