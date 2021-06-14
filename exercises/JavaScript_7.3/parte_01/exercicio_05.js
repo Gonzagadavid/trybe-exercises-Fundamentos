ab0bfed16b1eb6d5263d890476d627d7b09d45cf
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-06-14 18:21:40 -0300

    refatorado exercicios da primeira parte do 7.3

--- exercises/JavaScript_7.3/parte_01/exercicio_05.js
+++ exercises/JavaScript_7.3/parte_01/exercicio_05.js
@@ -19,5 +19,5 @@ const obj3 = {
 // implemente seus testes aqui
 
 assert.deepStrictEqual(obj1, obj2);
-assert.deepStrictEqual(obj1, obj3);
-assert.deepStrictEqual(obj2, obj3);
+assert.notDeepStrictEqual(obj1, obj3);
+assert.notDeepStrictEqual(obj2, obj3);
